7086acecb6fd8d351b86777cb8b31c6dbb25be06
HamzaRekik <rekikhamza11@gmail.com>
2023-09-30 20:05:42 +0200

    commit tefeh just sala7t size + singlechildview

--- tbibi/lib/views/signup_page.dart
+++ tbibi/lib/views/signup_page.dart
@@ -20,80 +20,85 @@ class SignUpPage extends StatelessWidget {
               fontWeight: FontWeight.w500),
         ),
       ),
-      body: Container(
-          child: Column(
-        children: [
-          Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.white38,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    InputText(hint: "First Name"),
-                    InputText(
-                      hint: "Last Name",
-                    ),
-                    InputText(
-                      hint: "Email",
-                    ),
-                    InputText(
-                      hint: "Password",
-                    ),
-                    InputText(
-                      hint: "Confirm Password",
-                    ),
-                    SignButton(
-                        text: "SIGN UP",
-                        textColor: Colors.white,
-                        backgroundColor: Color(0xFF4CE3B1),
-                        function: () {})
-                  ],
-                ),
-              )),
-          Expanded(
-              child: Container(
-            width: double.infinity,
-            color: Color(0xFF4163CD),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                "Are you a doctor ?",
-                style: TextStyle(
-                    fontFamily: "Poppins",
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "More than 100 patients search and find the right doctor for us each month.",
-                style: TextStyle(color: Colors.white, fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Join Now",
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Color(0xFF4163CD),
-                        fontFamily: 'Poppins Medium',
-                        fontWeight: FontWeight.w500),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))))
-            ]),
-          ))
-        ],
-      )),
+      body: SingleChildScrollView(
+        child: Container(
+            height: 728,
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.white38,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          InputText(hint: "First Name"),
+                          InputText(
+                            hint: "Last Name",
+                          ),
+                          InputText(
+                            hint: "Email",
+                          ),
+                          InputText(
+                            hint: "Password",
+                          ),
+                          InputText(
+                            hint: "Confirm Password",
+                          ),
+                          SignButton(
+                              text: "SIGN UP",
+                              textColor: Colors.white,
+                              backgroundColor: Color(0xFF4CE3B1),
+                              function: () {})
+                        ],
+                      ),
+                    )),
+                Expanded(
+                    child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  color: Color(0xFF4163CD),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Are you a doctor ?",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "More than 100 patients search and find the right doctor for us each month.",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Join Now",
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  color: Color(0xFF4163CD),
+                                  fontFamily: 'Poppins Medium',
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20))))
+                      ]),
+                ))
+              ],
+            )),
+      ),
     );
   }
 }
