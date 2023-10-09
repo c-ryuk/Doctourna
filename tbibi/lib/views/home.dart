c92d60208a6a5ff439ac38d285e8a022f73490f1
HamzaRekik <rekikhamza11@gmail.com>
2023-10-09 20:40:40 +0200

    fixed home page scroll

--- tbibi/lib/views/home.dart
+++ tbibi/lib/views/home.dart
@@ -55,102 +55,43 @@ class _DoctorsListPageState extends State<DoctorsListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              onChanged: _filterDoctors,
-              decoration: InputDecoration(
-                labelText: 'Search Doctors',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(
-                    color: Color(0xFF4163CD),
-                    width: 2.0,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                onChanged: _filterDoctors,
+                decoration: InputDecoration(
+                  labelText: 'Search Doctors',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                    width: 1.0,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      color: Color(0xFF4163CD),
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: Text(
-              "What are your issue?",
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 70,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: symptoms.length,
-              itemBuilder: (context, index) {
-                final currentSymptom = symptoms[index];
-                final isSelected = currentSymptom == selectedSymptom;
-
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (currentSymptom == selectedSymptom) {
-                        selectedSymptom = "";
-                      } else {
-                        selectedSymptom = currentSymptom;
-                      }
-                    });
-                  },
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    padding: EdgeInsets.symmetric(horizontal: 25),
-                    decoration: BoxDecoration(
-                      color: isSelected ? Color(0xFF4163CD) : Color(0xFFF4F6FA),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 4,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        currentSymptom,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: isSelected ? Colors.white : Colors.black54,
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 35),
-          Align(
-            alignment: Alignment.centerLeft,
+          SliverToBoxAdapter(child: SizedBox(height: 30)),
+          SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.only(left: 15),
               child: Text(
-                "Popular Doctors",
+                "What are your issue?",
                 style: TextStyle(
                   fontSize: 23,
                   fontWeight: FontWeight.w500,
@@ -158,19 +99,85 @@ class _DoctorsListPageState extends State<DoctorsListPage> {
               ),
             ),
           ),
-          SizedBox(height: 25),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.7,
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 70,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: symptoms.length,
+                itemBuilder: (context, index) {
+                  final currentSymptom = symptoms[index];
+                  final isSelected = currentSymptom == selectedSymptom;
+
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (currentSymptom == selectedSymptom) {
+                          selectedSymptom = "";
+                        } else {
+                          selectedSymptom = currentSymptom;
+                        }
+                      });
+                    },
+                    child: Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      decoration: BoxDecoration(
+                        color:
+                            isSelected ? Color(0xFF4163CD) : Color(0xFFF4F6FA),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          currentSymptom,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: isSelected ? Colors.white : Colors.black54,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-              itemCount: filteredDoctors.length,
-              itemBuilder: (context, index) {
-                final doctor = filteredDoctors[index];
-                return DoctorBox(doctor: doctor);
-              },
             ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 35)),
+          SliverToBoxAdapter(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Text(
+                  "Popular Doctors",
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 25)),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.7,
+            ),
+            delegate: SliverChildBuilderDelegate((context, index) {
+              final doctor = filteredDoctors[index];
+              return DoctorBox(doctor: doctor);
+            }, childCount: filteredDoctors.length),
           ),
         ],
       ),
--- tbibi/lib/views/login_page.dart
+++ tbibi/lib/views/login_page.dart
@@ -9,117 +9,96 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          height: 800,
-          child: Stack(
+          height: 820,
+          child: Column(
             children: [
-              Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      width: double.infinity,
-                      color: Color(0xFF4163CD),
-                      margin: EdgeInsets.only(top: 40),
-                      child: Image.asset(
-                        "assets/login_image.jpg",
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      width: double.infinity,
-                      color: Color(0xFF4163CD),
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          InputText(hint: "Email"),
-                          InputText(hint: "Password"),
-                          SizedBox(
-                            height: 26,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 125),
-                            child: Text(
-                              "Forgot your password ?",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Poppins',
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          SignButton(
-                            text: "LOGIN",
-                            textColor: Colors.white,
-                            backgroundColor: Color(0xFF4CE3B1),
-                            function: () {},
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "New Member ?",
-                            style: TextStyle(
-                              fontFamily: "Poppins",
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return SignUpPage();
-                              }));
-                            },
-                            child: Text(
-                              "SIGN IN",
-                              style: TextStyle(
-                                fontSize: 22,
-                                color: Colors.white,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: Size(310, 40),
-                              backgroundColor: Color(0xFF4163CD),
-                              shape: RoundedRectangleBorder(
-                                side: BorderSide(
-                                  color: Colors.white,
-                                  width: 1.0,
-                                ),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+              Expanded(
+                flex: 2,
+                child: Image.asset(
+                  "assets/login_image.jpg",
+                  fit: BoxFit.cover,
+                ),
               ),
-              Positioned(
-                top: 30,
-                left: 20,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Colors.white,
+              Expanded(
+                flex: 2,
+                child: Container(
+                  width: double.infinity,
+                  color: Color(0xFF4163CD),
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InputText(hint: "Email"),
+                      InputText(hint: "Password"),
+                      SizedBox(
+                        height: 26,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 125),
+                        child: Text(
+                          "Forgot your password ?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SignButton(
+                        text: "LOGIN",
+                        textColor: Colors.white,
+                        backgroundColor: Color(0xFF4CE3B1),
+                        function: () {},
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "New Member ?",
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return SignUpPage();
+                          }));
+                        },
+                        child: Text(
+                          "SIGN IN",
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(310, 40),
+                          backgroundColor: Color(0xFF4163CD),
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.white,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
                 ),
               ),
             ],
