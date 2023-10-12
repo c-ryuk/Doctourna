7b17efd178d66ace1509a0738badddc8322ea216
HamzaRekik <rekikhamza11@gmail.com>
2023-10-13 00:51:38 +0200

    added loading indicator

--- tbibi/lib/views/luncher_page.dart
+++ tbibi/lib/views/luncher_page.dart
@@ -4,10 +4,24 @@ class LunchingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Expanded(
-              child:
-                  Image(image: AssetImage("assets/doctourna_lunch_page.png")))),
-    );
+        body: Center(
+            child: Expanded(
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image(image: AssetImage("assets/doctourna_lunch_page.png")),
+        SizedBox(
+          height: 20,
+        ),
+        Transform.scale(
+          scale: 1.5,
+          child: CircularProgressIndicator(
+            backgroundColor: Colors.black,
+            color: Color(0xFF4163CD),
+            strokeWidth: 2,
+          ),
+        ),
+      ],
+    ))));
   }
 }
