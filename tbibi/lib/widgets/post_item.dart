f3a8f1c1759f83824868d57f28e4ba6deb04a549
missa999 <missaouim88@gmail.com>
2023-11-23 18:24:47 +0100

    correct some bugs

--- tbibi/lib/widgets/post_item.dart
+++ tbibi/lib/widgets/post_item.dart
@@ -1,17 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:tbibi/models/post.dart';
-import 'package:tbibi/static_data/users_list.dart';
 
 class PostBody extends StatefulWidget {
   final Function toggleTheme;
   final bool isDarkMode;
   const PostBody({
-    super.key,
+    Key? key,
     required this.post,
     required this.toggleTheme,
     required this.isDarkMode,
-  });
+  }) : super(key: key);
+
   final Map<String, dynamic> post;
 
   @override
@@ -19,15 +18,15 @@ class PostBody extends StatefulWidget {
 }
 
 class _PostBodyState extends State<PostBody> {
-  Map<String, dynamic> userData = {};
+  late Future<Map<String, dynamic>> _userDataFuture;
 
   @override
   void initState() {
     super.initState();
-    loadUserPost();
+    _userDataFuture = loadUserPost();
   }
 
-  Future<void> loadUserPost() async {
+  Future<Map<String, dynamic>> loadUserPost() async {
     try {
       FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -37,114 +36,134 @@ class _PostBodyState extends State<PostBody> {
       DocumentSnapshot snapshot = await firestore.doc(userDocumentPath).get();
 
       if (snapshot.exists) {
-        userData = snapshot.data() as Map<String, dynamic>;
-        setState(() {});
+        return snapshot.data() as Map<String, dynamic>;
       }
     } catch (error) {
-      print('Error updating profile: $error');
+      print('Error fetching user data: $error');
     }
+    return {}; // Return an empty map if fetching fails
   }
 
   void inSelect(BuildContext context) {
-  Navigator.of(context).pushNamed(
-    '/post-detail',
-    arguments: {
-      'post': widget.post,
-      'userData': userData,
-    },
-  ).then((value) {
-    if (value != null) {
-      print(value);
-    }
-  });
-}
-
+    Navigator.of(context).pushNamed(
+      '/post-detail',
+      arguments: {
+        'post': widget.post,
+        'userData': _userDataFuture,
+      },
+    ).then((value) {
+      if (value != null) {
+        print(value);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => inSelect(context),
-      child: Container(
-        margin: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: widget.isDarkMode ? Colors.black : Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              offset: Offset(0, 15),
-              blurRadius: 25,
-              color: Colors.black26,
-            ),
-          ],
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              child: DecoratedBox(
+    return FutureBuilder<Map<String, dynamic>>(
+      future: _userDataFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if (snapshot.hasError || !snapshot.hasData) {
+            return Center(child: Text('Error fetching data'));
+          } else {
+            Map<String, dynamic> userData = snapshot.data!;
+            return InkWell(
+              onTap: () => inSelect(context),
+              child: Container(
+                margin: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15)),
-                  image: DecorationImage(
-                    image: NetworkImage(widget.post['image']),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.5),
-                      BlendMode.darken,
+                  borderRadius: BorderRadius.circular(15),
+                  color: widget.isDarkMode ? Colors.black : Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      offset: Offset(0, 15),
+                      blurRadius: 25,
+                      color: Colors.black26,
                     ),
-                  ),
+                  ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text(
-                      widget.post['title'],
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(widget.post['image']),
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.5),
+                              BlendMode.darken,
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              widget.post['title'],
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 25,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  userData['image'] ?? '',
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(userData['username'] ?? ''),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.today,
+                                color: Color(0xFF4163CD),
+                              ),
+                              const SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                '${DateTime.parse(widget.post['dateTime']).year}-${DateTime.parse(widget.post['dateTime']).month.toString().padLeft(2, '0')}-${DateTime.parse(widget.post['dateTime']).day.toString().padLeft(2, '0')}',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: Image.network(userData['image']).image,
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(userData['username']),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.today,
-                        color: Color(0xFF4163CD),
-                      ),
-                      const SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                          '${DateTime.parse(widget.post['dateTime']).year}-${DateTime.parse(widget.post['dateTime']).month.toString().padLeft(2, '0')}-${DateTime.parse(widget.post['dateTime']).day.toString().padLeft(2, '0')}'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+            );
+          }
+        }
+      },
     );
   }
 }
